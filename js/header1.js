7b71b3854bf85e966d595e72217c14019ab55670
AbdalrhmanBanyissa <aboood.banyissa.94@gmail.com>
2021-05-04 21:32:04 +0300

    add final booking form

---
+++ js/header1.js
@@ -0,0 +1,15 @@
+
+'use strict';
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop + 100;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
